7d93157902e2c845f69ec19a6fb212d8cf1108df
PK_Van <pk.van@mail.ru>
2023-03-08 02:39:36 +0300

    yyy

--- src/main/resources/public/users.js
+++ src/main/resources/public/users.js
@@ -1,9 +1,6 @@
-// let userJSONData = JSON.stringify(data);
-// let userData = JSON.parse(userJSONData)
-//  console.log(userData.userId);
+const url = 'https://jsonplaceholder.typicode.com/posts';
 
-
-fetch('https://jsonplaceholder.typicode.com/posts')
+fetch(url)
     .then(response => response.json())
     .then(data => {
         if (Array.isArray(data)) {
@@ -11,29 +8,39 @@ fetch('https://jsonplaceholder.typicode.com/posts')
                 let userJSONData = JSON.stringify(data);
                 let userData = JSON.parse(userJSONData)
 
+
                 const userItem = document.createElement('div');
                 userItem.classList = "user_item flex";
 
-                const userItemText = document.createElement('p');
-                userItemText.classList.add('user_item_paragraph');
+
+                const userItemInfo = document.createElement('div');
+                userItemInfo.classList = "user_item_info flex";
+
 
                 document.querySelector('.users_conteiner').appendChild(userItem);
-                userItem.appendChild(userItemText);
-                userItemText.innerHTML = (
-                    "userId: " + userData.userId + "<br>" +
-                    "id: " + userData.id + "<br>" +
-                    "title: " + userData.title + "<br>" +
-                    "body: " + userData.body
-                );
+                userItem.appendChild(userItemInfo);
+                // userItemText.innerHTML = (
+                //     "userId: " + userData.userId + "<br>" +
+                //     "id: " + userData.id + "<br>" +
+                //     "title: " + userData.title + "<br>" +
+                //     "body: " + userData.body
+                // );
+
+                
 
                 const userItemRight = document.createElement('div');
                 userItemRight.className = "user_item_buttons flex";
                 userItem.appendChild(userItemRight);
 
+
                 const userItemUpdateBtn = document.createElement('button');
                 userItemUpdateBtn.classList.add('user_item_btn');
                 userItemUpdateBtn.textContent = "Редактировать";
                 userItemRight.appendChild(userItemUpdateBtn);
+                userItemUpdateBtn.addEventListener('click', function() {
+                    console.log(userItemInfo);
+                })
+
 
                 const userItemDeleteBtn = document.createElement('button');
                 userItemDeleteBtn.className = "user_item_btn user_item_delete_btn";
