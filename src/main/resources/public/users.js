76217cf94e9333bf52137d27f34df44a1fd1ccfa
PK_Van <pk.van@mail.ru>
2023-03-10 23:49:50 +0300

    Выгрузка боевого скрипта

--- src/main/resources/public/users.js
+++ src/main/resources/public/users.js
@@ -1,19 +1,16 @@
-const url = 'https://jsonplaceholder.typicode.com/posts';
-const deleteUserUrl = 'https://jsonplaceholder.typicode.com'
+const url = 'https://jsonplaceholder.typicode.com/posts'; //testURL
+const GETusersUrl = 'http://safg13.ddns.net:8080/user/list'
+const DELETEUserUrl = 'http://safg13.ddns.net:8080/user/'
 
-
-
-
-
-fetch(url)
+fetch(GETusersUrl)
     .then(response => response.json())
     .then(data => {
         if (Array.isArray(data)) {
             data.forEach(data => {
                 let userJSONData = JSON.stringify(data);
                 let userData = JSON.parse(userJSONData)
-
-
+            
+            
                 const userItem = document.createElement('div');
                 userItem.classList = "user_item flex";
                 document.querySelector('.users_conteiner').appendChild(userItem);
@@ -22,61 +19,118 @@ fetch(url)
                 const userItemInfo = document.createElement('div');
                 userItemInfo.classList = "user_item_info flex";
                 userItem.appendChild(userItemInfo);
-
-                const userId = document.createElement('div');
-                userId.classList = "item_info_userId";
-                userId.innerHTML = "<mark>userId: </mark>" + userData.userId
-                userItemInfo.appendChild(userId);
-
+            
                 const id = document.createElement('div');
                 id.classList = "item_info_userId";
                 id.innerHTML = "<mark>id: </mark>" + userData.id
                 userItemInfo.appendChild(id);
-
-                const title = document.createElement('div');
-                title.classList = "item_info_userId";
-                title.innerHTML = "<mark>title: </mark>" + userData.title
-                userItemInfo.appendChild(title);
-
-                const body = document.createElement('div');
-                body.classList = "item_info_userId";
-                body.innerHTML = "<mark>body: </mark>" + userData.body
-                userItemInfo.appendChild(body);
-
+            
+                const name = document.createElement('div');
+                name.classList = "item_info_userId";
+                name.innerHTML = "<mark>name: </mark>" + userData.name
+                userItemInfo.appendChild(name);
+            
+                const email = document.createElement('div');
+                email.classList = "item_info_userId";
+                email.innerHTML = "<mark>email: </mark>" + userData.email
+                userItemInfo.appendChild(email);
+            
+                const phone = document.createElement('div');
+                phone.classList = "item_info_userId";
+                phone.innerHTML = "<mark>phone: </mark>" + userData.phone
+                userItemInfo.appendChild(phone);
+            
+                const password = document.createElement('div');
+                password.classList = "item_info_userId";
+                password.innerHTML = "<mark>password: </mark>" + userData.password
+                userItemInfo.appendChild(password);
+            
                 const userItemRight = document.createElement('div');
                 userItemRight.className = "user_item_buttons flex";
                 userItem.appendChild(userItemRight);
-
+            
                 const userItemUpdateBtn = document.createElement('button');
                 userItemUpdateBtn.classList.add('user_item_btn');
                 userItemUpdateBtn.textContent = "Отправить в консоль";
                 userItemRight.appendChild(userItemUpdateBtn);
                 userItemUpdateBtn.addEventListener('click', function() {
-                    console.log(userId.innerHTML + ' ' + id.innerHTML + ' ' + title.innerHTML + ' ' + body.innerHTML);
+                    console.log(id.innerHTML + ' ' + name.innerHTML + ' ' + email.innerHTML + ' ' + phone.innerHTML + ' ' + password.innerHTML);
                 })
-
-
+            
+            
                 const userItemDeleteBtn = document.createElement('button');
                 userItemDeleteBtn.className = "user_item_btn user_item_delete_btn";
                 userItemDeleteBtn.textContent = "Удалить";
                 userItemRight.appendChild(userItemDeleteBtn);
                 userItemDeleteBtn.addEventListener('click', function(){
                     document.querySelector('.users_conteiner').removeChild(userItem);
-                    fetch((deleteUserUrl + '/' + userData.id), {
+                    fetch((DELETEUserUrl + userData.id), {
                         method: 'DELETE'
                     })
-                });
-
-
+                });           
             });
         } else {
             let userJSONData = JSON.stringify(data);
             let userData = JSON.parse(userJSONData)
-
+        
+        
             const userItem = document.createElement('div');
-            userItem.classList.add('user_item')
-            userItem.innerHTML = JSON.stringify(data);
+            userItem.classList = "user_item flex";
             document.querySelector('.users_conteiner').appendChild(userItem);
+            userItem.dataset.userid = userData.id; // Присваивание Data-UserId
+            
+            const userItemInfo = document.createElement('div');
+            userItemInfo.classList = "user_item_info flex";
+            userItem.appendChild(userItemInfo);
+        
+            const id = document.createElement('div');
+            id.classList = "item_info_userId";
+            id.innerHTML = "<mark>id: </mark>" + userData.id
+            userItemInfo.appendChild(id);
+        
+            const name = document.createElement('div');
+            name.classList = "item_info_userId";
+            name.innerHTML = "<mark>name: </mark>" + userData.name
+            userItemInfo.appendChild(name);
+        
+            const email = document.createElement('div');
+            email.classList = "item_info_userId";
+            email.innerHTML = "<mark>email: </mark>" + userData.email
+            userItemInfo.appendChild(email);
+        
+            const phone = document.createElement('div');
+            phone.classList = "item_info_userId";
+            phone.innerHTML = "<mark>phone: </mark>" + userData.phone
+            userItemInfo.appendChild(phone);
+        
+            const password = document.createElement('div');
+            password.classList = "item_info_userId";
+            password.innerHTML = "<mark>password: </mark>" + userData.password
+            userItemInfo.appendChild(password);
+        
+            const userItemRight = document.createElement('div');
+            userItemRight.className = "user_item_buttons flex";
+            userItem.appendChild(userItemRight);
+        
+            const userItemUpdateBtn = document.createElement('button');
+            userItemUpdateBtn.classList.add('user_item_btn');
+            userItemUpdateBtn.textContent = "Отправить в консоль";
+            userItemRight.appendChild(userItemUpdateBtn);
+            userItemUpdateBtn.addEventListener('click', function() {
+                console.log(id.innerHTML + ' ' + name.innerHTML + ' ' + email.innerHTML + ' ' + phone.innerHTML + ' ' + password.innerHTML);
+            })
+        
+        
+            const userItemDeleteBtn = document.createElement('button');
+            userItemDeleteBtn.className = "user_item_btn user_item_delete_btn";
+            userItemDeleteBtn.textContent = "Удалить";
+            userItemRight.appendChild(userItemDeleteBtn);
+            userItemDeleteBtn.addEventListener('click', function(){
+                document.querySelector('.users_conteiner').removeChild(userItem);
+                fetch((deleteUserUrl + '/' + userData.id), {
+                    method: 'DELETE'
+                })
+            });        
         }
     })
     .catch(error => {
