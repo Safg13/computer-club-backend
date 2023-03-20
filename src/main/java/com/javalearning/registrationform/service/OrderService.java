18121fe9a25bec2533392da08146196c95488fbe
mev <rtrickz@gmail.com>
2023-03-20 21:22:53 +0300

    Фикс сервиса order

--- src/main/java/com/javalearning/registrationform/service/OrderService.java
+++ src/main/java/com/javalearning/registrationform/service/OrderService.java
@@ -2,7 +2,9 @@ package com.javalearning.registrationform.service;
 
 import com.javalearning.registrationform.model.Order;
 import com.javalearning.registrationform.repository.OrderRepository;
+import org.springframework.stereotype.Service;
 
+@Service
 public class OrderService extends GenericService<Order> {
 
     private final OrderRepository repository;
