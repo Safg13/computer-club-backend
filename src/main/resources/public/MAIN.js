9fbc112f68c8f14bf3af3ca1bb986128986d3b25
PK_Van <pk.van@mail.ru>
2023-03-31 06:05:24 +0300

    Полный перенос страницы USERS на React

--- src/main/resources/public/MAIN.js
+++
@@ -1,7 +0,0 @@
-// ENDPOINTS //
-const MAINURL = 'http://safg13.ddns.net:8080'
-
-const GETusersUrl = MAINURL + '/user/list';
-const DELETEUserUrl = MAINURL + '/user/';
-const PUTUserUrl = MAINURL + '/user/';
-const POSTUserUrl = MAINURL + '/user/add';
